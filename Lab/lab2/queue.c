71768ea31375caf8bdc4df1d50bdc81b7ff71fda
cheems <yangry2023@shanghaitech.edu.cn>
2025-03-03 00:41:47 +0800

    add malloc realloc check

--- Lab/lab2/queue.c
+++ Lab/lab2/queue.c
@@ -4,22 +4,42 @@
 
 Queue *queue_create(void) {
   Queue *queue = malloc(sizeof(Queue));
+
+  if (!queue) {
+    printf("malloc failed for queue\n");
+    exit(EXIT_FAILURE);
+  }
+
   queue->size = 0;
   queue->capacity = QUEUE_INITIAL_CAPACITY;
   queue->data = malloc(sizeof(double) * queue->capacity);
+
+  if (!queue->data) {
+    printf("malloc failed for data\n");
+    free(queue);
+    exit(EXIT_FAILURE);
+  }
+
   return queue;
 }
 
 void push(Queue *queue, double element) {
   if (!queue) {
     printf("invalid queue\n");
-    return;
+    exit(EXIT_FAILURE);
   }
 
   if (queue->size == queue->capacity) {
     int capacity = queue->capacity * 2;
 
     queue->data = realloc(queue->data, sizeof(double) * capacity);
+
+    if (!queue->data) {
+      printf("realloc failed\n");
+      free(queue);
+      exit(EXIT_FAILURE);
+    }
+
     queue->capacity = capacity;
   }
 
@@ -29,33 +49,42 @@ void push(Queue *queue, double element) {
 }
 
 double back(Queue *queue) {
-  if (!queue || queue->size == 0) {
+  if (!queue) {
+    printf("invalid queue\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if (!queue->data) {
+    printf("invalid data\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if (queue->size == 0) {
     printf("no element in queue\n");
-    return 0.0;
+    exit(EXIT_FAILURE);
   }
 
   return queue->data[queue->size - 1];
 }
 
 void queue_free(Queue *queue) {
-  if (!queue) {
-    printf("invalid queue\n");
+  if (!queue)
     return;
-  }
 
-  free(queue->data);
+  if (queue->data)
+    free(queue->data);
+
   free(queue);
 }
 
 // avoid dangling pointer
 void another_queue_free(Queue **queue) {
-  if (!queue || !*queue) {
-    printf("invalid queue\n");
-    queue = NULL;
+  if (!queue || !*queue)
     return;
-  }
 
-  free((*queue)->data);
+  if ((*queue)->data)
+    free((*queue)->data);
+
   free(*queue);
   *queue = NULL;
 }
