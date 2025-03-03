e5c2f375b0cbcdf4bf02e9201837ad82d1e3c137
cheems <yangry2023@shanghaitech.edu.cn>
2025-03-04 02:06:18 +0800

    correct mistake

--- convert.c
+++ convert.c
@@ -1,27 +1,20 @@
 #include <stdio.h>
 
+enum type { normal, subnormal, zero, nan, inf };
+
 typedef struct {
   int sign;     // 1 bit
   int exponent; // 32 bits
   int mantissa; // 31 bits
+  enum type type;
 } ca25;
 
 typedef struct {
   int sign;     // 1 bit
   int exponent; // 8 bits
   int mantissa; // 23 bits
+  enum type type;
 } fp32;
-
-// convert ca25 to fp32
-fp32 convert(ca25 ca25) {
-  fp32 fp32;
-  fp32.sign = ca25.sign;
-  fp32.exponent = ca25.exponent + 127;
-  fp32.mantissa = ca25.mantissa << 8; // TODO:
-  return fp32;
-}
-
-enum type { normal, subnormal, zero, nan, inf };
 
 const char *getTypeName(enum type type) {
   switch (type) {
@@ -39,29 +32,60 @@ const char *getTypeName(enum type type) {
   return NULL;
 }
 
+// convert ca25 to fp32
+fp32 convert(ca25 ca25) {
+  fp32 fp32;
+  fp32.sign = ca25.sign;
+  fp32.type = ca25.type;
+
+  if (ca25.type == nan) {
+    fp32.exponent = 0xFF;
+    fp32.mantissa = ca25.mantissa >> 8;
+    return fp32;
+  }
+
+  if (ca25.type == inf || ca25.exponent > 127) { // Too large for fp32
+    fp32.type = inf;
+    fp32.exponent = 0xFF;
+    fp32.mantissa = 0;
+    return fp32;
+  }
+
+  if (ca25.type == zero) {
+    fp32.exponent = 0;
+    fp32.mantissa = 0;
+    return fp32;
+  }
+
+  // Convert normal/subnormal values
+
+  return fp32;
+}
+
 int main(void) {
   unsigned long long input;
   while (scanf("%llx", &input) != EOF) {
     ca25 ca25;
     ca25.sign = input >> 63;
     ca25.exponent = input >> 31 & 0xFFFFFFFF;
-    ca25.mantissa = input & 0x7FFFFFFF;
+    ca25.mantissa = (input & 0x7FFFFFFF) << 1; // one bit redundant, always zero
+    ca25.type = normal;
 
     int implicit = ca25.exponent == 0 ? 0 : 1;
 
-    enum type type = normal;
     if (implicit == 0)
-      type = ca25.mantissa == 0 ? zero : subnormal;
+      ca25.type = ca25.mantissa == 0 ? zero : subnormal;
     else if (ca25.exponent == (int)0xFFFFFFFF)
-      type = ca25.mantissa == 0 ? inf : nan;
+      ca25.type = ca25.mantissa == 0 ? inf : nan;
 
     printf("ca25 S=%d E=%08x M=%d.%08x %s\n", ca25.sign, ca25.exponent,
-           implicit, ca25.mantissa << 1, getTypeName(type));
+           implicit, ca25.mantissa, getTypeName(ca25.type));
 
     fp32 fp32 = convert(ca25);
 
     printf("fp32 S=%d E=%02x M=%d.%06x %s\n", fp32.sign, fp32.exponent,
-           implicit, fp32.mantissa, getTypeName(type));
+           implicit, fp32.mantissa,
+           getTypeName(fp32.type)); // TODO: whether -127 is correct
   }
 
   return 0;
